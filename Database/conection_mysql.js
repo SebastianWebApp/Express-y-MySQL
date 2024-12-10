ec00d69d04df9413165a0bcf58b8bd9250cef746
SebastianWebApp <mateoespinosam@hotmail.com>
2024-12-09 19:59:03 -0500

    Reintento de conexión a la base de datos

--- Database/conection_mysql.js
+++ Database/conection_mysql.js
@@ -14,12 +14,22 @@ async function testConnection() {
     const connection = await connectToDB.getConnection();
     console.log("Conexión exitosa a la base de datos!");
     connection.release(); // Liberar la conexión después de usarla
+
+    // Para crear la base de datos con su tabla
+    initializeDatabase();
+
+
   } catch (error) {
     console.error("Error al conectar a la base de datos:", error.message);
+
+    // Agregar un retraso de 5 segundos antes de intentar una acción adicional
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    
+    console.log("Reintentando conectar a la base de datos...");
+    // Aquí podrías intentar llamar a `testConnection` nuevamente o manejar la lógica de reintento
+    await testConnection();
+
   }
 }
 
 testConnection();
-
-// Para crear la base de datos con su tabla
-initializeDatabase();
